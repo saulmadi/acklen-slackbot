9e890e09c5d21238f98676206269cd6db2404127
Saul Mayorquin <saul@acklenavenue.com>
2015-04-17 19:21:48 -0600

    remove build

--- build/specs/trelloService.spec.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../typings/mocha/mocha.d.ts" />
-/// <reference path="../../typings/chai/chai.d.ts" />
-/// <reference path="../../typings/node/node.d.ts"/>
-/// <reference path="../../typings/chai-as-promised/chai-as-promised.d.ts" />
-var chai = require('chai');
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-var expect = chai.expect;

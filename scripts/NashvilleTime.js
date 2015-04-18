4ff70b6c7863331668e0ce5d504d5adc45c820d3
Byron Sommardahl <byron@acklenavenue.com>
2015-04-17 19:36:49 -0500

    Added taunt.

--- scripts/NashvilleTime.js
+++ scripts/NashvilleTime.js
@@ -18,7 +18,8 @@ function NashvilleTime(robot) {
         "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQu5Oxk-y5MmZVquRbeSzzngH6990LSh-K08D0BUcKUjvKNncsW",
         "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTmkNuEnmaC0nxr5CRb0gIGe0yggqUjZVvtCkiBszZNmZ9cvZ5QNg",
         "http://s2.quickmeme.com/img/9c/9cedc5066c87efc1c09dffe668c6adf919500a34b9cb337a3afff7aa1622f6fc.jpg",
-        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRy_LBFGoYQitLY2edYmRUnkljWMVtondEHrFObnbOfYs8IBsvQ_g"
+        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRy_LBFGoYQitLY2edYmRUnkljWMVtondEHrFObnbOfYs8IBsvQ_g",
+        "It's always 5 o'clock somewhere. But here, it's only 5 o'clock when it's 5 o'clock in Nashville.",        
     ];
     robot.hear(/time zone|Nashville Time|Honduran time|Honduras time|gringo time/i, function (msg) {
         msg.reply(msg.random(taunts));
